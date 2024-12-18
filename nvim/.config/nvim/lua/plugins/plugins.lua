f1ffbd511b4ef3813fd562e18b777ef04b2b1423
jqyDee <matti.fischbach@web.de>
2024-12-18 14:15:19 +0100

    added copilot package to nvim

--- nvim/.config/nvim/lua/plugins/plugins.lua
+++ nvim/.config/nvim/lua/plugins/plugins.lua
@@ -23,6 +23,9 @@ return {
   -- lsp
   "williamboman/mason.nvim",
   "williamboman/mason-lspconfig.nvim",
+  -- COPILOT
+  "zbirenbaum/copilot.lua",
+  -- COPILOT
   {
     "VonHeikemen/lsp-zero.nvim",
     branch = "v3.x"
