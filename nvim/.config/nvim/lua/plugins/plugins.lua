9eb30450fa17914d88d6c7c7830ffddd1a104df8
jqyDee <matti.fischbach@web.de>
2025-01-30 00:00:29 +0100

    fixed the gruvbox plugin commit

--- nvim/.config/nvim/lua/plugins/plugins.lua
+++ nvim/.config/nvim/lua/plugins/plugins.lua
@@ -2,6 +2,8 @@ return {
   -- colorscheme
   {
     "morhetz/gruvbox",
+    branch = "master",
+    commit = "f1ecde8",
     priority = 1000,
     config = function()
       vim.cmd("let g:gruvbox_contrast_dark = 'hard'")
--- nvim/.config/nvim/lua/user/set.lua
+++ nvim/.config/nvim/lua/user/set.lua
@@ -9,7 +9,7 @@ vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.smartindent = true
-vim.opt.expandtab = false
+vim.opt.expandtab = true
 
 vim.opt.wrap = true
 
